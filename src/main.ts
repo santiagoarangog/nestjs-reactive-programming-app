f01a00134bf848c6efaf157e2753877a5af721f8
Santiago Arango Gutierrez <santiago9606@gmail.com>
2025-07-25 23:38:12 -0500

    Doc: Add http-exception-filter

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import { HttpExceptionFilter } from './shared/infraestructure/http/http-exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -14,6 +15,8 @@ async function bootstrap() {
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
+
+  app.useGlobalFilters(new HttpExceptionFilter());
 
   await app.listen(3000);
 }
---
+++ src/shared/infraestructure/http/http-exception.filter.ts
@@ -0,0 +1,41 @@
+import { ExceptionFilter, Catch, ArgumentsHost, HttpException, HttpStatus } from '@nestjs/common';
+import { Response } from 'express';
+import { ApplicationException } from '../../domain/application.exception';
+
+@Catch()
+export class HttpExceptionFilter implements ExceptionFilter {
+    catch(exception: unknown, host: ArgumentsHost) {
+        const ctx = host.switchToHttp();
+        const response = ctx.getResponse<Response>();
+
+        let statusCode = HttpStatus.INTERNAL_SERVER_ERROR;
+        let message = 'Ha ocurrido un error inesperado en el servidor.';
+        let error = 'Internal Server Error';
+
+        if (exception instanceof ApplicationException) {
+            // Nuestras excepciones de negocio personalizadas
+            message = exception.message;
+            // Asignar un statusCode específico según el tipo de excepción
+            if (exception.name === 'ProductNotFoundException') {
+                statusCode = HttpStatus.NOT_FOUND;
+                error = 'Not Found';
+            } else {
+                // Otras excepciones de negocio pueden ser un Bad Request
+                statusCode = HttpStatus.BAD_REQUEST;
+                error = 'Bad Request';
+            }
+        } else if (exception instanceof HttpException) {
+            // Excepciones propias de NestJS (ej: las de validación con class-validator)
+            const httpResponse = exception.getResponse() as any;
+            statusCode = exception.getStatus();
+            message = httpResponse.message || exception.message;
+            error = httpResponse.error || 'Http Exception';
+        }
+
+        response.status(statusCode).json({
+            statusCode,
+            message,
+            error,
+        });
+    }
+}
